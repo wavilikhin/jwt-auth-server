6cd3fe9a0a332b4f49e610e5f82b7c90e32185ab
wavilikhin <vavilikhin.v@gmail.com>
2020-10-16 17:19:39 +0300

    🆖 Fixes Nginx

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
 require(`dotenv`).config();
 
-const https = require('https');
+// const https = require('https');
 const fs = require('fs');
 
 const express = require('express');
 const app = express();
 
-const options = {
-  key: fs.readFileSync('/srv/www/keys/my-site-key.pem'),
-  cert: fs.readFileSync('/srv/www/keys/chain.pem'),
-};
+// const options = {
+//   key: fs.readFileSync('/srv/www/keys/my-site-key.pem'),
+//   cert: fs.readFileSync('/srv/www/keys/chain.pem'),
+// };
 
-const whitelist = require('./config/cors.config');
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+// const whitelist = require('./config/cors.config');
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
 
 const PORT = process.env.PORT;
 
@@ -38,10 +38,10 @@ const errorsHandler = require('./source/middleware/errorsHandler');
 mongoose.connect(mongoConfig.url, mongoConfig.options);
 
 app.listen(PORT);
-https.createServer(options, app).listen(PORT);
+// https.createServer(options, app).listen(PORT);
 
 app.use(process.env.MODE === 'dev' ? morgan('dev') : morgan('combined'));
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(helmet());
 
 app.use('/auth', require('./source/routes/auth/auth'));
