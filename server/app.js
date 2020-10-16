5e6e3b3f25ccb050673495231a1fe550964678d0
wavilikhin <vavilikhin.v@gmail.com>
2020-10-16 16:51:34 +0300

    🙅‍♂️ Adds cors boilerplate and nginx conf for DDOS and Brute Force attacks prevention

--- server/app.js
+++ server/app.js
@@ -1,10 +1,30 @@
 require(`dotenv`).config();
 
+const https = require('https');
+const fs = require('fs');
+
 const express = require('express');
 const app = express();
 
+const options = {
+  key: fs.readFileSync('/srv/www/keys/my-site-key.pem'),
+  cert: fs.readFileSync('/srv/www/keys/chain.pem'),
+};
+
+const whitelist = require('./config/cors.config');
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 const PORT = process.env.PORT;
 
+const cors = require('cors');
 const mongoose = require('mongoose');
 const mongoConfig = require('./config/mongo.config');
 
@@ -18,8 +38,10 @@ const errorsHandler = require('./source/middleware/errorsHandler');
 mongoose.connect(mongoConfig.url, mongoConfig.options);
 
 app.listen(PORT);
+https.createServer(options, app).listen(PORT);
 
 app.use(process.env.MODE === 'dev' ? morgan('dev') : morgan('combined'));
+app.use(cors(corsOptions));
 app.use(helmet());
 
 app.use('/auth', require('./source/routes/auth/auth'));
