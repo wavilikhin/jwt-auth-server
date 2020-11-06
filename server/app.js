961e14669fa36943b9299e245a310c0a6b693d11
wavilikhin <vavilikhin.v@gmail.com>
2020-11-06 17:44:42 +0300

    Almost done with test setup

--- server/app.js
+++ server/app.js
@@ -1,56 +1,50 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-
-// const whitelist = require('./config/cors.config');
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
-
 const PORT = process.env.PORT;
 
-// const cors = require("cors");
 const mongoose = require('mongoose');
 const mongoConfig = require('./config/mongo.config');
 
-const helmet = require('helmet');
-const morgan = require('morgan');
-const jwtAuthStrategy = require('express-jwt');
-const guard = require('express-jwt-permissions')();
-const errorsHandler = require('./source/middleware/errorsHandler');
-
 mongoose.connect(mongoConfig.url, mongoConfig.options);
 
-app.listen(PORT);
-app.use(process.env.MODE === 'dev' ? morgan('dev') : morgan('combined'));
-// app.use(cors(corsOptions));
-app.use(helmet());
+function createApp() {
+  const express = require('express');
+  const app = express();
+  const helmet = require('helmet');
+  const morgan = require('morgan');
+  const jwtAuthStrategy = require('express-jwt');
+  const guard = require('express-jwt-permissions')();
+  const errorsHandler = require('./source/middleware/errorsHandler');
 
-// app.get('/test', (req, res) => {
-//   console.log('Handling request...');
-//   setTimeout(() => {
-//     console.log('Data collected');
-//   }, 3000);
-//   console.log('Request successfully fulfilled');
-//   res.json({ success: true }).status(200);
-// });
+  app.use(process.env.MODE === 'dev' ? morgan('dev') : morgan('combined'));
 
-app.use('/auth', require('./source/routes/auth/auth'));
-app.use(
-  jwtAuthStrategy({
-    secret: require('./config/jwt.config').accessTokenSecret,
-    algorithms: ['HS256'],
-  })
-);
-guard.check(['admin']);
-app.use('/users', require('./source/routes/users/users'));
-app.use(errorsHandler);
+  app.use(helmet());
 
-module.exports = app;
+  app.get('/', (req, res) => {
+    res.sendStatus(200);
+  });
+
+  app.use('/auth', require('./source/routes/auth/auth'));
+  app.use(
+    jwtAuthStrategy({
+      secret: require('./config/jwt.config').accessTokenSecret,
+      algorithms: ['HS256'],
+    })
+  );
+  guard.check(['admin']);
+  app.use('/users', require('./source/routes/users/users'));
+  app.use(errorsHandler);
+
+  return app;
+}
+
+// TODO: Подумать что делать с uncaught exeptions
+process.on('uncaughtException', (err) => {
+  console.log(err);
+});
+
+if (!module.parent) {
+  createApp().listen(PORT);
+}
+
+module.exports = createApp;
--- server/source/test/routes/auth.js
+++ server/source/test/routes/auth.js
@@ -1,83 +1,45 @@
 const test = require('ava');
 const request = require('supertest');
-const { MongoMemoryServer } = require('mongodb-memory-server');
+
 const mongoose = require('mongoose');
 const User = require('../../model/user');
-const app = require('../../../app');
+const mongoConfig = require('../../../config/mongo.config');
+
+const createApp = require('../../../app');
+const app = request(createApp());
 
 const { hashSync } = require('bcryptjs');
 const { v4: uuid } = require('uuid');
 
-const App = request(app);
-const mongod = new MongoMemoryServer();
-
-test.before(async () => {
-  mongoose.connect(await mongod.getUri(), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+test.before(() => {
+  console.log('BEFORE');
+  mongoose.connect(mongoConfig.testUrl, mongoConfig.options);
 });
 
 test.beforeEach(async () => {
-  const user1 = new User({
-    id: uuid(),
-    email: 'fakeOne@mail.com',
-    password: hashSync('fakepass123', 8),
-  });
-  const user2 = new User({
-    id: uuid(),
-    email: 'fakeTwo@mail.com',
-    password: hashSync('fakepass123', 8),
-  });
-  const user3 = new User({
-    id: uuid(),
-    email: 'fakeThree@mail.com',
-    password: hashSync('fakepass123', 8),
-  });
-  const user4 = new User({
-    id: uuid(),
-    email: 'fakeFour@mail.com',
-    password: hashSync('fakepass123', 8),
-  });
-  const user5 = new User({
-    id: uuid(),
-    email: 'fakeFive@mail.com',
-    password: hashSync('fakepass123', 8),
-  });
+  console.log('BEFORE EACH');
 
-  await user1.save();
-  await user2.save();
-  await user3.save();
-  await user4.save();
-  await user5.save();
+  const newUser = {
+    id: uuid(),
+    email: 'fake@mail.com',
+    password: hashSync('fakepass123', 8),
+  };
+
+  console.log('NEW USER\n', newUser);
+
+  await new User(newUser).save();
+
+  console.log('USER SAVED');
 });
 
-test.afterEach.always(async () => await User.deleteMany());
-
-test.after.always(async (t) => {
-  mongoose.disconnect();
-  mongod.stop();
+test.serial('User can successfuly sign in', async (t) => {
+  const signinResponse = await app.post('/auth/signin').send({
+    email: 'test@mail.com',
+    password: 'fake123',
+    confirmedPassword: 'fake123',
+  });
+  t.is(signinResponse.status, 201);
 });
-
-test.serial('passes serially', (t) => {
-  t.pass();
-});
-
-// test.serial('Test for test', async (t) => {
-//   const response = await App.get('/test');
-//   t.is(response.status, 200);
-//   t.truthy(typeof response.body.success === 'boolean');
-//   t.is(response.body.success, true);
-// });
-
-// test.serial('User can successfuly sign in', async (t) => {
-//   const signinResponse = await App.post('/auth/signin').send({
-//     email: 'test@mail.com',
-//     password: 'fake123',
-//     confirmedPassword: 'fake123',
-//   });
-//   t.is(signinResponse.status, 201);
-// });
 
 test.todo('User can successfuly log in');
 test.todo('User gets 403 on invalid cridentials');
@@ -87,3 +49,9 @@ test.todo('User gets 404 on invalid refresh token');
 test.todo('User can use refresh token only once');
 test.todo('Refresh tokens becomes invalid on logout');
 test.todo('Multiple refresh tokens are valid');
+
+// test.afterEach.always(async () => await User.deleteMany());
+
+test.after.always(async () => {
+  mongoose.disconnect();
+});
